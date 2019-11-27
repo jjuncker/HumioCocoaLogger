8057ff8254be415bcdeb5532411b71921c3f236b
mratrifork <37136867+mratrifork@users.noreply.github.com>
2019-11-27 20:35:14 +0100

    Update to swift5 (#8)
    * Update to swift5
    
    * Update podspec and dependencies
    
    * Update filename to OS

--- Project/Pods/Target Support Files/CocoaLumberjack/CocoaLumberjack-umbrella.h
+++ Project/Pods/Target Support Files/CocoaLumberjack/CocoaLumberjack-umbrella.h
@@ -1,19 +1,26 @@
+#ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#endif
 
+#import "CLIColor.h"
 #import "DDAbstractDatabaseLogger.h"
 #import "DDASLLogCapture.h"
 #import "DDASLLogger.h"
 #import "DDAssertMacros.h"
-#import "DDFileLogger.h"
-#import "DDLegacyMacros.h"
-#import "DDLog+LOGV.h"
-#import "DDLog.h"
-#import "DDLogMacros.h"
-#import "DDTTYLogger.h"
-#import "CocoaLumberjack.h"
 #import "DDContextFilterLogFormatter.h"
 #import "DDDispatchQueueLogFormatter.h"
+#import "DDFileLogger+Buffering.h"
+#import "DDFileLogger.h"
+#import "DDLog+LOGV.h"
+#import "DDLog.h"
+#import "DDLoggerNames.h"
+#import "DDLogMacros.h"
 #import "DDMultiFormatter.h"
+#import "DDOSLogger.h"
+#import "DDTTYLogger.h"
+#import "CocoaLumberjack.h"
+#import "DDLegacyMacros.h"
+#import "SwiftLogLevel.h"
 
 FOUNDATION_EXPORT double CocoaLumberjackVersionNumber;
 FOUNDATION_EXPORT const unsigned char CocoaLumberjackVersionString[];
